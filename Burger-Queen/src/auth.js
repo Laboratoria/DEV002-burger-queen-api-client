1957e8cb9871f945eb71fd3fb50a40e9e5a10f1a
Ariel-Navarro <arielknm@gmail.com>
2023-03-22 01:35:05 -0500

    Hace la petición con axios a localhost/login (backend), modificación de sintaxis para la petición de axios.

--- Burger-Queen/src/auth.js
+++ Burger-Queen/src/auth.js
@@ -1,35 +1,36 @@
+import axios from "axios";
+
 const auth = async ({email, password}) => {
     let token = null;
-    await fetch('http://127.0.0.1:5173/', {
+     return await axios.post('http://127.0.0.1:8080/login', {email, password}, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json',
-        },
-        body: JSON.stringify({ email, password})
-    })
-    .then(res => {
-        token = {
-            ...token,
-            ok: false,
-            status: res.status
         }
-        return res.json()
     })
-    .then(res => {
-        token = {
-            ok:(token.status == 200) ? true : false,
-            message: (token.status == 200) ? 'Login' : res,
-            token: (token.status == 200) ? res : null
-        };
-    })
-    .catch(err => {
-        token = {
-            ok: false,
-            message: 'Error of login',
-            token: null
-        }
-    });
-    return token;
+    // .then(res => {
+    //     token = {
+    //         ...token,
+    //         ok: false,
+    //         status: res.status
+    //     }
+    //     return res.json()
+    // })
+    // .then(res => {
+    //     token = {
+    //         ok:(token.status == 200) ? true : false,
+    //         message: (token.status == 200) ? 'Login' : res,
+    //         token: (token.status == 200) ? res : null
+    //     };
+    // })
+    // .catch(err => {
+    //     token = {
+    //         ok: false,
+    //         message: 'Error of login',
+    //         token: null
+    //     }
+    // });
+    // return token;
 }
 
 export {
--- Burger-Queen/src/components/Login.jsx
+++ Burger-Queen/src/components/Login.jsx
@@ -1,31 +1,39 @@
-// import { useState } from 'react'
 import '../estilos/login/login.css'
-
+import { auth } from '../../src/auth.js';
+import { useNavigate } from 'react-router-dom';
 import React, { Fragment, useState } from 'react';
 import logo from "./img/imgLogo.jpg";
 import lyricsLogo from "./img/lyricsLogo.jpg";
 import referenceFood from "./img/referenceFood.png";
 
 const Login = () => {
+  const onNavigate = useNavigate();
+  const [credentials, setCredentials] = useState({ email: '', password: '' });
+  const [error, setError] = useState('');
 
-  const [datos, setDatos] = useState({
-    email: '',
-    password: ''
-  })
+  const { email, password } = credentials;
 
   const handleInputChange = (event) => {
-    // console.log(event.target.name)
-    // console.log(event.target.value)
-    setDatos({
-      ...datos,
-      [event.target.name]: event.target.value
-    })
-  }
+    const { name, value } = event.target;
+    setCredentials({ ...credentials, [name]: value });
+  };
 
-  const enviarDatos = (event) => {
-    event.preventDefault()
-    console.log('enviando datos...' + datos.nombre + ' ' + datos.apellido)
-  }
+  const enviarDatos = async (event) => {
+    event.preventDefault();
+    try {
+      const response = await auth({ email, password });
+      console.log('token', response);
+      
+      if (response.data.ok === true) {
+        localStorage.setItem('token', response.data.token);
+        // redirigir a la página principal
+        onNavigate('/admin')
+      }
+    } catch (error) {
+      setError(error.response.data.message);
+    }
+  };
+1  
 
   return (
     <Fragment>
@@ -40,17 +48,18 @@ const Login = () => {
             <div className="col-md-3">
               <label>
                 Email:
-                <input type="email" placeholder="Email" className="form-control" onChange={handleInputChange} name="email"></input>
+                <input type="email" placeholder="Email" className="form-control" onChange={handleInputChange} value={email} name="email"></input>
               </label>
             </div>
             <div className="col-md-3">
               <label>
                 Password:
-                <input type="password" placeholder="Password" className="form-control" onChange={handleInputChange} name="password"></input>
+                <input type="password" placeholder="Password" className="form-control" onChange={handleInputChange}value={password} name="password"></input>
               </label>
             </div>
             <button type="submit" className="loginbtn">Login</button>
           </form>
+          {error && <div className="alert alert-danger">{error}</div>}
           <div>
             <img src={referenceFood} alt="referenceFood" />
           </div>
@@ -59,5 +68,60 @@ const Login = () => {
     </Fragment>
   );
 }
+
+
+// const Login = () => {
+
+//   const [datos, setDatos] = useState({
+//     email: '',
+//     password: ''
+//   })
+
+//   const handleInputChange = (event) => {
+//     // console.log(event.target.name)
+//     // console.log(event.target.value)
+//     setDatos({
+//       ...datos,
+//       [event.target.name]: event.target.value
+//     })
+//   }
+
+//   const enviarDatos = (event) => {
+//     event.preventDefault()
+//     console.log('enviando datos...' + datos.nombre + ' ' + datos.apellido)
+//   }
+
+//   return (
+//     <Fragment>
+//       <div className='logo'>
+//         <img className='lyrics-logo' src={lyricsLogo} alt="lyricsLogo" />
+//         <img className='img-logo' src={logo} alt="logo" />
+//       </div>
+//       <h1 id='WAY'>Who are you?</h1>
+//       <div className='loginContent'>
+//         <section className='form'>
+//           <form className="row" onSubmit={enviarDatos}>
+//             <div className="col-md-3">
+//               <label>
+//                 Email:
+//                 <input type="email" placeholder="Email" className="form-control" onChange={handleInputChange} name="email"></input>
+//               </label>
+//             </div>
+//             <div className="col-md-3">
+//               <label>
+//                 Password:
+//                 <input type="password" placeholder="Password" className="form-control" onChange={handleInputChange} name="password"></input>
+//               </label>
+//             </div>
+//             <button type="submit" className="loginbtn">Login</button>
+//           </form>
+//           <div>
+//             <img src={referenceFood} alt="referenceFood" />
+//           </div>
+//         </section>
+//       </div>
+//     </Fragment>
+//   );
+// }
 
 export default Login;
