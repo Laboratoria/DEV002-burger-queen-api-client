64946a67061b5441510481d12c10cf9e52e86f73
Ariel-Navarro <arielknm@gmail.com>
2023-03-25 02:31:12 -0500

    Avance de llamao a la API para traer datos de los productos

--- Burger-Queen/src/components/card.jsx
+++ Burger-Queen/src/components/card.jsx
@@ -2,14 +2,15 @@ import React from "react";
 import burger from "./img/hamburguesa.png"
 import trash from "./img/basura.png"
 import edit from "./img/editar.png"
+import products from "../../../../burger-queen-api-mock/db.json"
 
 
-
-const Card = () => {
+const Card = ({name, image}) => {
+    console.log({name})
     return (
         <div className="card">
-            <div className="card-tittle">MY TITTLE</div>
-            <img className="img-card" src={burger}></img>
+            <div className="card-tittle">{name}</div>
+            <img className="img-card" src={image}></img>
             <div className="card-Body">
                <img className="trash" src={trash}></img>
                <img className="edit" src={edit}></img>
--- Burger-Queen/src/components/cards.jsx
+++ Burger-Queen/src/components/cards.jsx
@@ -1,52 +1,46 @@
 import React from "react";
 import Card from "../components/card.jsx"
 
+const cards = [
+    {
+        "id": 1,
+        "name": "Sandwich de jamón y queso",
+        "price": 1000,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/sandwich.png",
+        "type": "Desayuno",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 2,
+        "name": "Café americano",
+        "price": 500,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/coffee.png",
+        "type": "Desayuno",
+        "dateEntry": "2022-03-05 15:14:10"
+    },
+    {
+        "id": 3,
+        "name": "Agua 500ml",
+        "price": 500,
+        "image": "https://raw.githubusercontent.com/ssinuco/burger-queen-api-mock/main/resources/images/water.png",
+        "type": "Almuerzo",
+        "dateEntry": "2022-03-05 15:14:10"
+    }
+]
+
 const Cards = () => {
     return (
         <div>
             <div className="grid-cards">
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
-                <div className="row">
-                    <Card />
-                </div>
+
+                {
+                    cards.map(card => (
+                        <div key={card.name}>
+                        <Card tittle={card.name} />    
+                        </div>
+                    ))
+                }
+
             </div>
         </div>
     )
