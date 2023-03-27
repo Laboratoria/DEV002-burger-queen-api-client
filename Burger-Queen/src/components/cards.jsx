a274636dc6a52051944820995c0fbfbfa34a4c9e
Ariel-Navarro <arielknm@gmail.com>
2023-03-26 22:39:08 -0500

    Muestra los datos enlas cartas de los productos

--- Burger-Queen/src/components/cards.jsx
+++ Burger-Queen/src/components/cards.jsx
@@ -4,39 +4,75 @@ import Card from "../components/card.jsx"
 import axios from "axios";
 
 const Cards = () => {
-    const token = localStorage.getItem("token");
-    const [cardsData, setCardsData] = useState([]);
-  
-    const getData = async () => {
-      const urlBurguerApi = "http://localhost:8080/products";
-      await axios
-        .get(urlBurguerApi, {
-          headers: {
-            Authorization: "Bearer " + token, //the token is a variable which holds the token
-          },
-        })
-        .then((response) => {
-          const data = response.data;
-          setCardsData(data);
-        });
-    };
-  
-    useEffect(() => {
-      getData();
-    }, []);
-  
-    return (
-      <div>
-        <div className="grid-cards">
-          {cardsData.map((card) => (
-            <div key={card.id}>
-              <Card />
-            </div>
-          ))}
-        </div>
-      </div>
-    );
+  const token = localStorage.getItem("token");
+  const [cardsData, setCardsData] = useState([]);
+
+  const getData = async () => {
+    const urlBurguerApi = "http://localhost:8080/products";
+    await axios
+      .get(urlBurguerApi, {
+        headers: {
+          Authorization: "Bearer " + token,
+        },
+      })
+      .then((response) => {
+        const data = response.data;
+        setCardsData(data);
+      });
   };
+
+  useEffect(() => {
+    getData();
+  }, []);
+
+  return (
+    <div>
+      <div className="grid-cards">
+        {cardsData.map((product) => (
+          <div key={product.id}>
+            <Card name={product.name} image={product.image} price={product.price} type={product.type} />
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+
+// const Cards = () => {
+//     const token = localStorage.getItem("token");
+//     const [cardsData, setCardsData] = useState([]);
+  
+//     const getData = async () => {
+//       const urlBurguerApi = "http://localhost:8080/products";
+//       await axios
+//         .get(urlBurguerApi, {
+//           headers: {
+//             Authorization: "Bearer " + token, //the token is a variable which holds the token
+//           },
+//         })
+//         .then((response) => {
+//           const data = response.data;
+//           setCardsData(data);
+//         });
+//     };
+  
+//     useEffect(() => {
+//       getData();
+//     }, []);
+  
+//     return (
+//       <div>
+//         <div className="grid-cards">
+//           {cardsData.map((card) => (
+//             <div key={card.id}>
+//               <Card />
+//             </div>
+//           ))}
+//         </div>
+//       </div>
+//     );
+//   };
   
 
 
