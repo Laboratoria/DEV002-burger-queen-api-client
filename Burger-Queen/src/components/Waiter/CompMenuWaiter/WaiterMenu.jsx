c232457050938e8d385ab1ca26642fea2f16f9b5
Ariel-Navarro <arielknm@gmail.com>
2023-04-29 15:53:30 -0500

    Ya funciona correctamente la lógica para mostrar la suma total del pedido.

--- Burger-Queen/src/components/Waiter/CompMenuWaiter/WaiterMenu.jsx
+++ Burger-Queen/src/components/Waiter/CompMenuWaiter/WaiterMenu.jsx
@@ -1,26 +1,17 @@
+import increase from "../../img/increase.png"
+import decrease from "../../img/decrease.png"
+import cancel from "../../img/cancel.png"
 import React from "react";
 import NavigationBarWaiter from "../NavigationBarWaiter";
 import "../../../estilos/admin/navigationBar.css";
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import Logotype from "../../../components/Admin/logotype.jsx"
 import OnlyButtonsWaiter from "../OnlyButtonsWaiter";
 import trash from "../../img/trash.png"
 import CardProductWaiter from "../CardProductWaiter";
-import OrderTaked from "../OrderTaked";
 import { GetProducts } from "../../../request";
 
 const Waiter = () => {
-
-    const [totalPrice, setTotalPrice] = useState(0);
-
-    // Función para actualizar el precio total
-    const updateTotalPrice = () => {
-        const total = selectedProducts.reduce(
-            (acc, curr) => acc + curr.price,
-            0
-        );
-        setTotalPrice(total);
-    };
 
     // ------------------------
 
@@ -51,6 +42,41 @@ const Waiter = () => {
         );
         setProductsWaiter(filteredProducts);
     };
+
+    const [sumaT, setSumaT] = useState();
+
+    const [productQuantity, setProductQuantity] = useState({});
+
+    const handleDecrease = (productId) => {
+        setProductQuantity((prevQuantity) => ({
+            ...prevQuantity,
+            [productId]: (prevQuantity[productId] || 1) - 1,
+        }));
+    };
+
+    const handleIncrease = (productId) => {
+        setProductQuantity((prevQuantity) => ({
+            ...prevQuantity,
+            [productId]: (prevQuantity[productId] || 1) + 1,
+        }));
+    };
+
+    const handleCancel = (productId) => {
+        setProductQuantity((prevQuantity) => ({
+            ...prevQuantity,
+            [productId]: 0,
+        }));
+        setSelectedProducts((prevProducts) =>
+            prevProducts.filter((product) => product.id !== productId)
+        );
+    };
+
+    useEffect(() => {
+        const prices = selectedProducts.map(product => product.price * (productQuantity[product.id] || 1));
+        const total = prices.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        setSumaT(total);
+    }, [selectedProducts, productQuantity]);
+
 
     return (
         <div className="container">
@@ -112,14 +138,55 @@ const Waiter = () => {
                                 </form>
                             </section>
                             <div className="ListOrdersWaiter">
-                                <OrderTaked 
-                                selectedProducts={selectedProducts} 
-                                setSelectedProducts={setSelectedProducts} 
-                                onUpdateTotalPrice={updateTotalPrice}
-                                />
+                                <>
+                                    {selectedProducts.map((product) => (
+                                        <div className="orderTaked" key={product.id}>
+                                            <div className="descriptionProduct">
+                                                <div
+                                                    id="iconOrderTaked"
+                                                    className="CardProductWaiter"
+                                                >
+                                                    <p>{product.name}</p>
+                                                    <img width={'40px'} src={product.image} alt="product" />
+                                                    <p>s/ {product.price}</p>
+                                                </div>
+                                            </div>
+                                            <div>
+                                                <p className="nameProductOrder">{product.name}</p>
+                                                <p className="price">S/ {product.price * (productQuantity[product.id] || 1)}</p>
+                                            </div>
+                                            <div className="iconosProductSelected">
+                                                <div>
+                                                    <img
+                                                        className="iconOrderTaked"
+                                                        width={'25px'}
+                                                        src={decrease}
+                                                        onClick={() => handleDecrease(product.id)}
+                                                    />
+                                                    <p style={{ color: "white" }}>{productQuantity[product.id] || 1}</p>
+                                                    <img
+                                                        className="iconOrderTaked"
+                                                        width={'25px'}
+                                                        src={increase}
+                                                        onClick={() => handleIncrease(product.id)}
+                                                    />
+                                                </div>
+                                                <div>
+                                                    <img
+                                                        className="iconOrderTaked"
+                                                        width={'25px'}
+                                                        src={cancel}
+                                                        onClick={() => handleCancel(product.id)}
+                                                    />
+                                                </div>
+                                            </div>
+                                            
+                                        </div>
+                                    ))}
+                                </>
                             </div>
                             <div>
-                                <p className="cost">Costo total: S/ {totalPrice}</p>
+                                <p className="cost">Costo total: S/ {sumaT}</p>
                             </div>
                             <div>
                                 <button id="inputSendWaiter" className="inputWaiter">Enviar al chef</button>
--- Burger-Queen/src/components/Waiter/OrderTaked.jsx
+++ Burger-Queen/src/components/Waiter/OrderTaked.jsx
@@ -1,18 +1,13 @@
-import { useState } from 'react';
+import { useState, useEffect } from 'react';
 import decrease from "../img/decrease.png"
 import increase from "../img/increase.png"
 import cancel from "../img/cancel.png"
 
 const OrderTaked = ({ selectedProducts, setSelectedProducts }) => {
-
-
+    
+    const [sumaT, setSumaT] = useState();
 
     const [productQuantity, setProductQuantity] = useState({});
-
-
-    // const handleClick = (product) => {
-    //     // Aquí puedes agregar cualquier otra lógica que necesites al hacer clic en la carta del producto.
-    // }
 
     const handleDecrease = (productId) => {
         setProductQuantity((prevQuantity) => ({
@@ -26,7 +21,6 @@ const OrderTaked = ({ selectedProducts, setSelectedProducts }) => {
             ...prevQuantity,
             [productId]: (prevQuantity[productId] || 1) + 1,
         }));
-        console.log('cantidad', productQuantity)
     };
 
     const handleCancel = (productId) => {
@@ -39,47 +33,29 @@ const OrderTaked = ({ selectedProducts, setSelectedProducts }) => {
         );
     };
 
-    // Tratando que se guarde un array todos los 
-
-    // const [costoTotal, setCostoTotal] = useState(0);
-
-    // const sumaTotal = () => {
-    //     let arrSumaTotal = product.id;
-    //     products.forEach((product) => {
-    //         arrSumaTotal += product.price * (productQuantity[product.id] || 1);
-    //     });
-    //     setCostoTotal(arrSumaTotal);
-    // };
-
-    // llama a SumaTotal en algún momento para actualizar el estado de costoTotal
+    useEffect(() => {
+        const prices = selectedProducts.map(product => product.price * (productQuantity[product.id] || 1));
+        const total = prices.reduce((accumulator, currentValue) => accumulator + currentValue, 0);
+        setSumaT(total);
+    }, [selectedProducts, productQuantity]);
 
     return (
         <>
-            {/* Estructura de las cartas que se van eligiendo para tomar la orden */}
             {selectedProducts.map((product) => (
                 <div className="orderTaked" key={product.id}>
                     <div className="descriptionProduct">
                         <div
                             id="iconOrderTaked"
                             className="CardProductWaiter"
-                            onClick={() => handleClick(product)}
                         >
                             <p>{product.name}</p>
                             <img width={'40px'} src={product.image} alt="product" />
-                            {/* Este precio no se modifica */}
                             <p>s/ {product.price}</p>
                         </div>
                     </div>
                     <div>
                         <p className="nameProductOrder">{product.name}</p>
-                        {/* Aquí tengo que mostrar un numero que será el resultado de product.price multiplicado por el número que representa {productQuantity[product.id] || 1} en la etiqueta <p> */}
                         <p className="price">S/ {product.price * (productQuantity[product.id] || 1)}</p>
-
-                        {/* {costoTotal == product.id ? 
-                            <p>{product.id}</p>:
-                            <div>Precio total: S/ {costoTotal}</div>
-                        } */}
-
                     </div>
                     <div className="iconosProductSelected">
                         <div>
@@ -108,8 +84,123 @@ const OrderTaked = ({ selectedProducts, setSelectedProducts }) => {
                     </div>
                 </div>
             ))}
+            <div onClick={handleSumT}>
+               <p>Total: S/ {sumaT}</p> 
+            </div>
         </>
     )
 };
+
+
+// const OrderTaked = ({ selectedProducts, setSelectedProducts }) => {
+
+//     // Se tiene que guardar en el un array los valores de {product.price * (productQuantity[product.id] || 1)} por id de producto para
+//     // hacer un .reduce y devolver el resultado de todos los valores.
+//     const [sumaT, setSumaT] = useState()
+
+//     const [productQuantity, setProductQuantity] = useState({});
+
+
+//     // const handleClick = (product) => {
+//     //     // Aquí puedes agregar cualquier otra lógica que necesites al hacer clic en la carta del producto.
+//     // }
+
+//     const handleDecrease = (productId) => {
+//         setProductQuantity((prevQuantity) => ({
+//             ...prevQuantity,
+//             [productId]: (prevQuantity[productId] || 1) - 1,
+//         }));
+//     };
+
+//     const handleIncrease = (productId) => {
+//         setProductQuantity((prevQuantity) => ({
+//             ...prevQuantity,
+//             [productId]: (prevQuantity[productId] || 1) + 1,
+//         }));
+//         console.log('cantidad', productQuantity)
+//     };
+
+//     const handleCancel = (productId) => {
+//         setProductQuantity((prevQuantity) => ({
+//             ...prevQuantity,
+//             [productId]: 0,
+//         }));
+//         setSelectedProducts((prevProducts) =>
+//             prevProducts.filter((product) => product.id !== productId)
+//         );
+//     };
+
+//     // Tratando que se guarde un array todos los 
+
+//     // const [costoTotal, setCostoTotal] = useState(0);
+
+//     // const sumaTotal = () => {
+//     //     let arrSumaTotal = product.id;
+//     //     products.forEach((product) => {
+//     //         arrSumaTotal += product.price * (productQuantity[product.id] || 1);
+//     //     });
+//     //     setCostoTotal(arrSumaTotal);
+//     // };
+
+//     // llama a SumaTotal en algún momento para actualizar el estado de costoTotal
+
+//     return (
+//         <>
+//             {/* Estructura de las cartas que se van eligiendo para tomar la orden */}
+//             {selectedProducts.map((product) => (
+//                 <div className="orderTaked" key={product.id}>
+//                     <div className="descriptionProduct">
+//                         <div
+//                             id="iconOrderTaked"
+//                             className="CardProductWaiter"
+//                             onClick={() => handleClick(product)}
+//                         >
+//                             <p>{product.name}</p>
+//                             <img width={'40px'} src={product.image} alt="product" />
+//                             {/* Este precio no se modifica */}
+//                             <p>s/ {product.price}</p>
+//                         </div>
+//                     </div>
+//                     <div>
+//                         <p className="nameProductOrder">{product.name}</p>
+//                         {/* Aquí tengo que mostrar un numero que será el resultado de product.price multiplicado por el número que representa {productQuantity[product.id] || 1} en la etiqueta <p> */}
+//                         <p className="price">S/ {product.price * (productQuantity[product.id] || 1)}</p>
+
+//                         {/* {costoTotal == product.id ? 
+//                             <p>{product.id}</p>:
+//                             <div>Precio total: S/ {costoTotal}</div>
+//                         } */}
+
+//                     </div>
+//                     <div className="iconosProductSelected">
+//                         <div>
+//                             <img
+//                                 className="iconOrderTaked"
+//                                 width={'25px'}
+//                                 src={decrease}
+//                                 onClick={() => handleDecrease(product.id)}
+//                             />
+//                             <p style={{ color: "white" }}>{productQuantity[product.id] || 1}</p>
+//                             <img
+//                                 className="iconOrderTaked"
+//                                 width={'25px'}
+//                                 src={increase}
+//                                 onClick={() => handleIncrease(product.id)}
+//                             />
+//                         </div>
+//                         <div>
+//                             <img
+//                                 className="iconOrderTaked"
+//                                 width={'25px'}
+//                                 src={cancel}
+//                                 onClick={() => handleCancel(product.id)}
+//                             />
+//                         </div>
+//                     </div>
+//                 </div>
+//             ))}
+//         </>
+//     )
+// };
 
 export default OrderTaked;
